cfa3181241040b3850ee163101cfe44e8dcb71e0
luka177 <lukapanio@gmail.com>
2023-02-25 11:36:23 +0100

    Add new lk support

--- lvgl.mk
+++ lvgl.mk
@@ -1,3 +1,4 @@
+ifdef OBJS
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/demos/lv_demos.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/examples/lv_examples.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/core/lv_core.mk
@@ -7,4 +8,9 @@ include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/font/lv_font.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal/lv_hal.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/misc/lv_misc.mk
 include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/widgets/lv_widgets.mk
-
+else
+MODULE_SRCS += $(shell find $(LVGL_PATH)/src -type f -name '*.c')
+MODULE_SRCS += $(shell find $(LVGL_PATH)/demos -type f -name '*.c')
+MODULE_SRCS += $(shell find $(LVGL_PATH)/examples -type f -name '*.c')
+GLOBAL_INCLUDES += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)/"
+endif
