e4607289fb905f758a2b23f3357f4ff74e423d6d
luka177 <lukapanio@gmail.com>
2023-02-25 11:36:23 +0100

    Add new lk support

--- lvgl.mk
+++ lvgl.mk
@@ -1,6 +1,13 @@
 LVGL_PATH ?= $(LVGL_DIR)/$(LVGL_DIR_NAME)
 
+ifdef OBJS
 OBJS += $(shell find $(LVGL_PATH)/src -type f -name '*.c' | sed 's/\.c/\.o/g')
 OBJS += $(shell find $(LVGL_PATH)/demos -type f -name '*.c' | sed 's/\.c/\.o/g')
 OBJS += $(shell find $(LVGL_PATH)/examples -type f -name '*.c' | sed 's/\.c/\.o/g')
 INCLUDES += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)/"
+else
+MODULE_SRCS += $(shell find $(LVGL_PATH)/src -type f -name '*.c')
+MODULE_SRCS += $(shell find $(LVGL_PATH)/demos -type f -name '*.c')
+MODULE_SRCS += $(shell find $(LVGL_PATH)/examples -type f -name '*.c')
+GLOBAL_INCLUDES += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)/"
+endif
