40943026d5df222d0c9e9914873c56eadb39d8af
luka177 <lukapanio@gmail.com>
2023-10-01 15:13:08 +0200

    Fix building on older lks, and simplify it

--- lvgl.mk
+++ lvgl.mk
@@ -1,13 +1,5 @@
 ifdef OBJS
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/demos/lv_demos.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/examples/lv_examples.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/core/lv_core.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/draw/lv_draw.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/extra/lv_extra.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/font/lv_font.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal/lv_hal.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/misc/lv_misc.mk
-include $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/widgets/lv_widgets.mk
+OBJS += $(shell find  $(LVGL_DIR)/$(LVGL_DIR_NAME)/src -name \*.c | sed 's/\.c/\.o/g')
 else
 MODULE_SRCS += $(shell find $(LVGL_PATH)/src -type f -name '*.c')
 MODULE_SRCS += $(shell find $(LVGL_PATH)/demos -type f -name '*.c')
--- src/hal/lv_hal.mk
+++
@@ -1,8 +0,0 @@
-OBJS += $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal/lv_hal_disp.o
-OBJS += $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal/lv_hal_indev.o
-OBJS += $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal/lv_hal_tick.o
-
-DEPPATH += --dep-path $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal
-VPATH += :$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal
-
-INCLUDES += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/hal"
